3ce501991eaef13d20f2c526be5008dfdb7fc049
ANUSHA CHINNI <anushac2345@outlook.com>
2025-11-05 15:23:39 -0500

    Object creation

---
+++ src/assignments/assignment3/EmpGroups.java
@@ -0,0 +1,26 @@
+package assignments.assignment3;
+
+
+public class EmpGroups {
+
+	public static void main(String[] args) {
+		String[] employeeNames = new String[3];
+		 int[] employeeIds = new int[3];
+		 
+		 Employees employees = new Employees();
+		 employeeNames[0] = employees.empName1;
+		 employeeIds[0] = employees.empId1;
+		 employeeNames[1] = employees.empName2;
+		 employeeIds[1] = employees.empId2;
+		 employeeNames[2] = employees.empName3;
+		 employeeIds[2] = employees.empId3;
+		 
+		 System.out.println("Employee Name: "+ employeeNames[0]+", Employee ID: "+ employeeIds[0]);
+		 System.out.println("Employee Name: "+ employeeNames[1]+", Employee ID: "+ employeeIds[1]);
+		 System.out.println("Employee Name: "+ employeeNames[2]+", Employee ID: "+ employeeIds[2]);
+
+	
+
+	}
+
+}
---
+++ src/assignments/assignment3/Employees.java
@@ -0,0 +1,14 @@
+package assignments.assignment3;
+
+public class Employees {
+
+	String empName1= "Bharath";
+	String empName2= "John";
+	String empName3= "Aarav";
+	int empId1 = 1234;
+	int empId2 = 1235;
+	int empId3 = 1236;  
+	
+
+
+}
